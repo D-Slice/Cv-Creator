720643fb88cff4383e33b9edcfb2d9a28915370a
D-Slice <tking4102@gmail.com>
2022-07-28 21:57:01 +0200

    testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'cv';
+  ngOnInit():void{
+    $('h1').click(function(){
+      console.log('jquery works');
+    })
+  }
 }
