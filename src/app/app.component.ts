718a1676808ae46e1214b01852269600b9f04dc4
D-Slice <tking4102@gmail.com>
2022-07-28 20:18:20 +0200

    test

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import * as $ from 'jquery';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'cv';
+}
